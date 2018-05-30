2fe1fc6f90efd4e8c0ce216d16ea5f697dfce267
Wiktor Olejniczak <myfrom.13th@gmail.com>
2018-05-30 18:56:07 +0200

    Update bug warning to include proper message

--- src/js/bugwarning.js
+++ src/js/bugwarning.js
@@ -1,33 +1,16 @@
 (() => {
   function callback() {
-    Notifier.showDialog(`⚠ Issues in Firefox and Edge`, `
+    Notifier.showDialog(`⚠ Issues in Edge`, `
       <paper-dialog-scrollable>
-        We are aware of a few bugs appearing in Edge and Firefox which might prevent you from editing the config.
-        You still can use a confgi that you downloaded eariler.
-        Currently, there are no known bugs in Chrome, Opera and Samsung Internet.
-        We are very sory for the inconvenience.
-        The bugs should be fixed by Wednesday or eariler.
+        There's currently a display bug in Edge. The app is usable but may look weird. We're apologising and working on a fix.
       </paper-dialog-scrollable>
       <div class="buttons">
         <paper-button autofocus dialog-confirm>Ok</paper-button>
       </div>
     `, { formatted: true })
   }
-
-
-  const isChromium = window.chrome,
-        winNav = window.navigator,
-        vendorName = winNav.vendor,
-        isIEedge = winNav.userAgent.indexOf("Edge") > -1,
-        isIOSChrome = winNav.userAgent.match("CriOS");
-
-  if (!(
-    isIOSChrome || (
-    isChromium !== null &&
-    typeof isChromium !== "undefined" &&
-    vendorName === "Google Inc." &&
-    isIEedge === false))
-  ) {
+  
+  if (navigator.userAgent.indexOf("Edge") > -1) {
     if ('Notifier' in window)
       callback()
     else
